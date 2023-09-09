a68eeec4be184a555d2504c4b537d708235f5273
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-09 21:37:38 +0700

    add title in head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
+    <div>
+      <Head>
+        <title>Fahmi Learn Next</title>
+      </Head>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ipsam,
         fugit quaerat sit numquam iste quos minima excepturi tempore labore.
       </p>
-    </>
+    </div>
   );
 }
