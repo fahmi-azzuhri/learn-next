811f4498c59b9b812fa385a8c89099f71bcdb33f
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2023-09-09 21:50:11 +0700

    custom error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <img src="/not_found.svg" alt="" />
+      <h1>Halaman Tidak ditemukan</h1>
+    </div>
+  );
+};
+
+export default ErrorPage;
